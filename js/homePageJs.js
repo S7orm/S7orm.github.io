872243bb5ac6960df798714eff1daaff369da759
S7orm <scotttpalermo@gmail.com>
2025-07-04 10:42:43 -0500

    main page upload

---
+++ js/homePageJs.js
@@ -0,0 +1,6 @@
+
+function pmpn(){
+var pmpArray = ["The Poor Man's Polymath", "Dunnning-Kruger in Action!"];
+var pmp=Math.round(Math.random(pmpArray.length));
+    document.getElementById('pmp').innerHTML = (pmpArray[pmp]);}
+pmpn();
